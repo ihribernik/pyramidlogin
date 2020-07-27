95549a947262d1ceda906bccc2501dce1a90e904
ivan hribernik <cihribernik@gmail.com>
2020-07-27 00:15:44 -0300

    se corrigio el home.jinja2,cssy el app.js

--- pyramidlogin/static/app.js
+++ pyramidlogin/static/app.js
@@ -1,130 +1,64 @@
-lista = [];
-$(document).ready(function () {
+let lista = [];
+$(document).ready(function (e) {
   cargarGrilla();
+  refrescarGrilla();
   $(document).on("click", "#submit", (e) => {
-    var objeto = new Object();
-    objeto.inputNombre = $("#inputNombre").val();
-    objeto.inputDescripcion = $("#inputDescripcion").val();
-    objeto.inputPrecio = $("#inputPrecio").val();
-    objeto.inputCantidad = $("#inputCantidad").val();
-    var jsonSend = JSON.stringify(objeto);
-    var http = new XMLHttpRequest();
-    var url = "php/insert-process.php";
-    var params = jsonSend;
-    http.open("POST", url, true);
-    //Send the proper header information along with the request
-    http.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    http.onreadystatechange = function () {
-      //Call a function when the state changes.
-      if (http.readyState == 4 && http.status == 200) {
-        var response = jQuery.parseJSON(http.responseText);
-        if (response.Errors) {
-          $("#alertEstado").removeClass("hide");
-          $("#alertEstado").addClass(
-            "alert alert-danger alert-dismissible show"
-          );
-          $("#alertEstado").text(response.Data);
-          $("#alertEstado").append(`
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>`);
-        } else {
-          $("#alertEstado").removeClass("hide");
-          $("#alertEstado").addClass(
-            "alert alert-success alert-dismissible show"
-          );
-          $("#alertEstado").text(response.Data);
-          $("#alertEstado").append(`
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>`);
-        }
-      }
-    };
-    http.send(params);
-  });
-
-  $(document).on("click", "#btn_modificar", (e) => {
-    $("#alertEstado").removeClass("hide");
-    $("#alertEstado").addClass("alert alert-success alert-dismissible show");
-    $("#alertEstado").text("agregando texto");
-    $("#alertEstado").append(`
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>`);
+    var nuevo = new Object();
+    nuevo.id = lista.length + 1;
+    nuevo.nombre = $("#inputNombre").val();
+    nuevo.descripcion = $("#inputDescripcion").val();
+    nuevo.precio = $("#inputPrecio").val();
+    nuevo.cantidad = $("#inputCantidad").val();
+    lista.push(nuevo);
+    alertify.success("se agrego correctamente un instrumento");
+    refrescarGrilla();
   });
 
   $(document).on("click", "#btn_eliminar", (e) => {
     if (confirm("seguro que quiere borrar el elemento seleccionado?")) {
       const element = $(this)[0].activeElement.parentElement.parentElement;
-      const id = $(element).attr("ElementoId");
-      var http = new XMLHttpRequest();
-      var url = "php/delete-process.php";
-      var objeto = new Object();
-      objeto.id = id;
-      var jsonSend = JSON.stringify(objeto);
-      var params = jsonSend;
-      http.open("POST", url, true);
-      //Send the proper header information along with the request
-      http.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-      http.onreadystatechange = function () {
-        //Call a function when the state changes.
-        if (http.readyState == 4 && http.status == 200) {
-          var response = jQuery.parseJSON(http.responseText);
-          if (response.Errors) {
-            $("#alertEstado").removeClass("hide");
-            $("#alertEstado").addClass(
-              "alert alert-danger alert-dismissible show"
-            );
-            $("#alertEstado").text(response.Data);
-            $("#alertEstado").append(`
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>`);
-          } else {
-            $("#alertEstado").removeClass("hide");
-            $("#alertEstado").addClass(
-              "alert alert-success alert-dismissible show"
-            );
-            $("#alertEstado").text(response.Data);
-            $("#alertEstado").append(`
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>`);
-          }
-          refrescarGrilla();
+      const id = $(element).attr("id");
+      let marca = false;
+      for (let index = 0; index < lista.length; index++) {
+        const objLista = lista[index];
+        if (objLista.id === id) {
+          lista.splice(index, 1);
+          marca = true;
         }
-      };
-      http.send(params);
+      }
+      if (marca) {
+        alertify.success("Se elimino correctamente");
+      } else {
+        alertify.alert("no se pudo borrar!");
+      }
     }
+    refrescarGrilla();
   });
 });
 
 function cargarGrilla() {
   for (var i = 1; i < 20; i++) {
     dict = {};
-    dict["id"] = i;
+    dict["id"] = "bs" + i;
     dict["nombre"] = "instrumento n°:" + i;
     dict["descripcion"] = "descripcion del instrumento n°" + i;
     dict["precio"] = Math.floor(Math.random() * 80000 + 1);
     dict["cantidad"] = Math.floor(Math.random() * 500 + 1);
     lista.push(dict);
   }
+}
 
+function refrescarGrilla() {
+  // limpio el elemento para que no se agrege objetos de mas
+  $("#tbodyInstrumentos").empty();
   lista.forEach((element) => {
-    var $borrar = $("<button>")
+    const $borrar = $("<button>")
       .attr("id", "btn_eliminar")
       .attr("type", "button")
       .addClass("btn btn-large btn-block btn-danger")
       .attr("name", "btn_eliminar")
       .text("borrar");
-    var $modificar = $("<button>")
-      .attr("id", "btn_modificar")
-      .attr("type", "button")
-      .addClass("btn btn-large btn-block btn-warning")
-      .attr("name", "btn_modificar")
-      .text("modificar");
-    var $tr = $("<tr>")
+    const $tr = $("<tr>")
       .attr("id", element.id)
       .append(
         $("<td>").text(element.id),
@@ -132,8 +66,7 @@ function cargarGrilla() {
         $("<td>").text(element.descripcion),
         $("<td>").text(element.precio),
         $("<td>").text(element.cantidad),
-        $("<td>").append($borrar),
-        $("<td>").append($modificar)
+        $("<td>").append($borrar)
       )
       .appendTo("#tbodyInstrumentos");
   });
